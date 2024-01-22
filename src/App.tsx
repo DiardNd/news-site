face00f35b7efff3ab02af250970c0d8c4480551
DiardNd <Diardbereg@mail.ru>
2024-01-22 12:19:36 +0300

    Authorization form

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Auth } from './components/Auth/Auth'
+import { Front } from './components/Front/Front'
+
+function App() {
+
+
+  return (
+    <>
+        {/* <Auth></Auth> */}
+        <Front></Front>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import styles from './Auth.module.scss'
+
+export interface AuthProps {
+}
+
+export const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const[emailError, setEmailError] = useState('')
+  
+  
+  const isValidEmail = (email:string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return(emailRegex.test(email))
+  }
+
+  const handleSign = () => {
+    if(!isValidEmail(email))
+    {
+      alert('Please enter a valid email')
+      setEmailError('incorrectEmail');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+  }
+  
+  return (
+
+<form className={styles.form}>
+      <div className={styles.blockInput}>
+        <input className={styles.input} 
+        name= "email"
+        // required
+        placeholder={emailError ? emailError : 'Email'}
+        onChange={(e)=>{
+          setEmail(e.target.value);
+          setEmailError('');
+        }}
+        autoComplete='off'>
+        </input>
+
+        <input className={styles.input} 
+        name= "password" 
+        // required 
+        placeholder='Password'
+        onChange={(e)=>{setPassword(e.target.value)}}
+        autoComplete='off'>
+        </input>
+      </div>
+
+      <div className={styles.blockButton}>
+        <button className={styles.button} onClick={handleSign}>Sign In</button>
+        <button className={styles.button} onClick={()=>{}}>Register</button>
+      </div>
+
+</form>
+    
+  );
+}
