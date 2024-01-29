de9ae65e92894dca5c76021b15384967221ba5f9
DiardNd <Diardbereg@mail.ru>
2024-01-29 11:20:53 +0300

    styles for header&menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,43 @@
-import react, { useState } from 'react';
-import styles from './Front.module.scss'
+import react, { useState } from 'react'
+import styles from './Header.module.scss'
 import lupa from '../../assets/icon-lupa.png'
 import menu from '../../assets/icon-menu.png'
 import { Menu } from '../Menu/Menu'
+import { Link } from 'react-router-dom'
 
+export const Header = () => {
+	const [hideSearch, setHideSearch] = useState(false)
+	const [hideMenu, setHideMenu] = useState(false)
 
-
-
-
-export const Front = () => {
-
-const [hideSearch, setHideSearch] = useState(false);
-const [hideMenu, setHideMenu] = useState(false)
-
-return(
-  <>
-  <div className={styles.container}>
-    <span className={styles.header}>News site</span>
-    <input className={hideSearch ? styles.hiddenSearchbar : styles.searchbar} type="text"
-    placeholder='Search'
-     />
-    <button className={styles.button} onClick={()=>setHideSearch(!hideSearch)}>
-      <img className={styles.icon} src={lupa} alt="icon-search"  />
-      </button>
-      <button className={styles.button} onClick={()=>{setHideMenu(!hideMenu)}}> 
-      <img className={styles.icon} src={menu} alt="icon-menu" /> 
-      </button>
-      <Menu hide={hideMenu}></Menu>
-  </div>
-  </>
-)
-
+	return (
+		<>
+			<div className={styles.container}>
+				<Link to='/' className={styles.header}>
+					News site
+				</Link>
+				<input
+					className={hideSearch ? styles.hiddenSearchbar : styles.searchbar}
+					type='text'
+					placeholder='Search'
+				/>
+				<div className={styles.iconContainer}>
+					<button
+						className={styles.button}
+						onClick={() => setHideSearch(!hideSearch)}
+					>
+						<img className={styles.icon} src={lupa} alt='icon-search' />
+					</button>
+					<button
+						className={styles.button}
+						onClick={() => {
+							setHideMenu(!hideMenu)
+						}}
+					>
+						<img className={styles.icon} src={menu} alt='icon-menu' />
+					</button>
+				</div>
+				<Menu hide={hideMenu}></Menu>
+			</div>
+		</>
+	)
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,17 @@
 import styles from './Menu.module.scss'
 
-
-
 export interface MenuProps {
-  hide: boolean
+	hide: boolean
 }
 
-
-
-
-export const Menu = ({hide}:MenuProps) =>{
-
-
-  return(
-    <div className={hide ? styles.container : styles.hidden}>
-<ul className={styles.menu}>
-  <li>Главная</li>
-  <li>Тема</li>
-  <li>Аккаунт</li>
-</ul>
-    </div>
-  )
-
+export const Menu = ({ hide }: MenuProps) => {
+	return (
+		<div className={hide ? styles.container : styles.hidden}>
+			<ul className={styles.menu}>
+				<li>Главная</li>
+				<li>Тема</li>
+				<li>Аккаунт</li>
+			</ul>
+		</div>
+	)
 }
